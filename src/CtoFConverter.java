896def1de4d2d172c87ec3cfe7cbfee4757fe490
Stephen Acheampong <acheamse@mail.uc.edu>
2022-07-10 17:45:06 -0400

    Basic Working State #1

--- src/CtoFConverter.java
+++ src/CtoFConverter.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class CtoFConverter {
     public static void main(String[] args) {
 
+        double C = 0;
+        double F = 0;
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter temperature in Celsius:");
+        C = in.nextDouble();
+
+        if (C == 0)
+        {
+            System.out.println("You have entered the freezing point");
+        }
+        else if (C == 100)
+        {
+            System.out.println("You have entered the boiling point");
+        }
+        else
+        {
+            F = C * 9 / 5 + 32;
+            System.out.println("The equivalent temperature in Fahrenheit is: " +F);
+        }
     }
 }
